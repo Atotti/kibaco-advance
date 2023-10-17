3ed4eeaa31785619a0a7157f160c26861643005e
Ayuto <Ayuto1215@outlook.com>
2023-10-18 00:07:28 +0900

    提出確認ページ判定を強化

--- preview.js
+++ preview.js
@@ -1,24 +1,28 @@
 // ページが読み込まれたときに実行される関数
 function addPdf() {
+    // 確認ページか判定
+    let progres = document.querySelector("#col1 > div > div > div.progress-container > div:nth-child(2) > div");
+    if (progres.style.width == "33.3333%") {
 
-    // PDFを見つける
-    let pdf_doc = document.querySelector("#col1 > div > div > ul > li > a");
+      // PDFを見つける
+      let pdf_doc = document.querySelector("#col1 > div > div > ul > li > a");
+      
+      // PDFが見つかったら表示
+      if (String(pdf_doc).includes(".pdf")) {
+        // PDFを表示するためのiframe要素を作成する
+        let iframe = document.createElement("iframe");
+        iframe.src = pdf_doc;
+        iframe.width = "100%";
+        iframe.height = "800px";
+        iframe.style.border = "none";
     
-    // PDFが見つかったら表示
-    if (String(pdf_doc).includes(".pdf")) {
-      // PDFを表示するためのiframe要素を作成する
-      let iframe = document.createElement("iframe");
-      iframe.src = pdf_doc;
-      iframe.width = "100%";
-      iframe.height = "800px";
-      iframe.style.border = "none";
-  
-      // PDFを追加する場所を見つける
-      let target = document.querySelector("#col1 > div > div");
-  
-      // iframe要素を追加する
-      target.appendChild(iframe);
-    }
+        // PDFを追加する場所を見つける
+        let target = document.querySelector("#col1 > div > div");
+    
+        // iframe要素を追加する
+        target.appendChild(iframe);
+      }
+  }
 }
   
 // ページが読み込まれたときにaddPdf関数を実行する
