06e40c53d58c24285c1e19968271593ba5489b28
Ayuto <Ayuto1215@outlook.com>
2024-04-23 16:04:07 +0900

    fix

--- notification.js
+++ notification.js
@@ -3,17 +3,19 @@ function notification() {
     if (document.querySelector("#col2of2 > div > nav > h2").textContent != '大学からのお知らせ') {
         return false;
     }
-    displayAnnouncements();
+    const url = document.querySelector("#toolMenu > ul > li:nth-child(5) > a").href;
+    const id = url.substring(url.lastIndexOf('/') + 1);
+    displayAnnouncements(id);
 }
 
-function displayAnnouncements() {
+function displayAnnouncements(url) {
     const segment = document.createElement('div');
     segment.className = 'Mrphs-container Mrphs-sakai-mytask';
 
     segment.innerHTML = `
 
 <div class="Mrphs-toolBody Mrphs-toolBody--sakai-mytask">
-<iframe name="Mainb913589b-f709-43ac-9211-2435364e04e1" id="Mainb913589b-f709-43ac-9211-2435364e04e1" title="お知らせ " class="portletMainIframe" height="100000" width="100%" frameborder="0" marginwidth="0" marginheight="0" scrolling="auto" allowfullscreen="allowfullscreen" allow="autoplay *;fullscreen *;encrypted-media *;camera 'self';microphone 'self'" src="https://kibaco.tmu.ac.jp/portal/tool/b913589b-f709-43ac-9211-2435364e04e1?panel=Main" style="height: 10000px;">
+<iframe name="Main${id}" id="Main${id}" title="お知らせ " class="portletMainIframe" height="100000" width="100%" frameborder="0" marginwidth="0" marginheight="0" scrolling="auto" allowfullscreen="allowfullscreen" allow="autoplay *;fullscreen *;encrypted-media *;camera 'self';microphone 'self'" src="https://kibaco.tmu.ac.jp/portal/tool/${id}?panel=Main" style="height: 10000px;">
 </iframe>
 </div>
 
