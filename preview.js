91e9aa499ade3c978b3ba34172a365243edf47f4
Ayuto Tsutsumi <56724676+Atotti@users.noreply.github.com>
2024-09-05 16:57:08 +0900

    リファクタ

--- preview.js
+++ preview.js
@@ -1,46 +1,50 @@
 // ページが読み込まれたときに実行される関数
 function addPdf() {
     // 確認ページか判定
-    let progres = document.querySelector("#col1 > div > div > div.progress-container > div:nth-child(2) > div");
-    let disp = document.querySelector("#col1 > div > div > h3").textContent;
-    if (progres.style.width == "33.3333%" && disp.includes("提出確認")) {
+    try {
+      let progres = document.querySelector("#col1 > div > div > div.progress-container > div:nth-child(2) > div");
+      let disp = document.querySelector("#col1 > div > div > h3").textContent;
+      if (progres.style.width == "33.3333%" && disp.includes("提出確認")) {
 
-      // PDFを見つける
-      let pdf_doc = document.querySelector("#col1 > div > div > ul > li > a");
+        // PDFを見つける
+        let pdf_doc = document.querySelector("#col1 > div > div > ul > li > a");
+        
+        // PDFが見つかったら表示
+        if (String(pdf_doc).slice(-4) == ".pdf") {
+          // PDFを表示するためのiframe要素を作成する
+          let iframe = document.createElement("iframe");
+          iframe.src = pdf_doc;
+          iframe.width = "100%";
+          iframe.height = "800px";
+          iframe.style.border = "4px solid green";
       
-      // PDFが見つかったら表示
-      if (String(pdf_doc).slice(-4) == ".pdf") {
-        // PDFを表示するためのiframe要素を作成する
-        let iframe = document.createElement("iframe");
-        iframe.src = pdf_doc;
-        iframe.width = "100%";
-        iframe.height = "800px";
-        iframe.style.border = "4px solid green";
+          // PDFを追加する場所を見つける
+          let target = document.querySelector("#col1 > div > div");
+      
+          // iframe要素を追加する
+          target.appendChild(iframe);
+          // png, jpg画像の場合も表示させる
+        } else if((String(pdf_doc).slice(-4) == ".png") || (String(pdf_doc).slice(-4) == ".jpg" )) {
+          // 画像を表示するためのimg要素を作成する
+          let img = document.createElement("img");
+          img.src = pdf_doc;
+          img.width = "100%";
+          img.height = "800px";
+          img.style.border = "4px solid green";
+          // 丸くする
+          img.style.borderRadius = "10px";
+          // css指定用のクラスを追加
+          img.className = "preview"
+      
+          // 画像を追加する場所を見つける
+          let target = document.querySelector("#col1 > div > div");
+      
+          // img要素を追加する
+          target.appendChild(img);
+        }
+    }
+  } catch {
     
-        // PDFを追加する場所を見つける
-        let target = document.querySelector("#col1 > div > div");
-    
-        // iframe要素を追加する
-        target.appendChild(iframe);
-        // png, jpg画像の場合も表示させる
-      } else if((String(pdf_doc).slice(-4) == ".png") || (String(pdf_doc).slice(-4) == ".jpg" )) {
-        // 画像を表示するためのimg要素を作成する
-        let img = document.createElement("img");
-        img.src = pdf_doc;
-        img.width = "100%";
-        img.height = "800px";
-        img.style.border = "4px solid green";
-        // 丸くする
-        img.style.borderRadius = "10px";
-        // css指定用のクラスを追加
-        img.className = "preview"
-    
-        // 画像を追加する場所を見つける
-        let target = document.querySelector("#col1 > div > div");
-    
-        // img要素を追加する
-        target.appendChild(img);
-      }
   }
 }
   
